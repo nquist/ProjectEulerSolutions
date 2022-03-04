6c93e3cbc529204929741b02309c44ebb9793a6c
nquist <47543479+nquist@users.noreply.github.com>
2022-03-03 22:35:24 -0800

    More problems

--- Projects 021-030/euler022.py
+++ Projects 021-030/euler022.py
@@ -3,22 +3,32 @@
 Created on Sat Jan 29 17:00:27 2022
 
 @author: nquist
-This solves project Euler problem 22. 
+This solves project Euler problem 22. This finds the sum of all the name scores
+from the file 'names.txt'. Each name score is calculated by finding the product
+of position value of the name in the alphabetial list multiplied by the sum
+of the value of each letter in the name.
 
-Answer: 
-Execution Time:  seconds
-
-
-    IN PROGRESS ----- NOT COMPLETE
+Answer: 871198282
+Execution Time: 0.02499 seconds
 """
-
 import time
 start = time.time()
 
+list_sum = 0
+
 f = open('names.txt', 'r')
+names = f.readlines()
+f.close()
+names = names[0].replace('"', '').split(',')
+names.sort()
 
-
-
+for i in range(len(names)):
+    char_sum = 0
+    for j in names[i]:
+        char_sum += ord(j)-64
+    list_sum += (i+1)*char_sum    
+    
+print('The total name score of the file is %i' % list_sum)
 
 end = time.time()
 print("The time of execution of above program is %.05f seconds" % (end-start))
---
+++ Projects 051-060/euler058.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 26 18:32:45 2022
+
+@author: nquist
+This solves project Euler problem 58. 
+
+Answer: 
+Execution Time:  seconds
+
+
+    IN PROGRESS ----- NOT COMPLETE
+"""
+
+import time
+start = time.time()
+
+diag_primes = 1
+square_primes = 1
+
+def find_primes():
+    return True
+
+
+def find_diag(max_val):
+    for i in range(2, 1001, 2):
+        for j in range(0, 4):
+            curr_num += i
+            total_sum += curr_num
+
+
+
+
+end = time.time()
+print("The time of execution of above program is %.05f seconds" % (end-start))
+
